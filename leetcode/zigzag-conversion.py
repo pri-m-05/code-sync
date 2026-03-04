f9bce57513fef220c75d37f1fea122b868f19f1d
Priyanka Mamman <priyanka.sandhumamman@gmail.com>
2026-03-04 09:36:36 -0500

    Add solution for zigzag-conversion

---
+++ leetcode/zigzag-conversion.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        rows = [""] * numRows
+        cur_row = 0
+        going_down = False
+        
+        for ch in s:
+            rows[cur_row] += ch
+            
+            if cur_row == 0 or cur_row == numRows - 1:
+                going_down = not going_down
+            
+            cur_row += 1 if going_down else -1
+        
+        return "".join(rows)
